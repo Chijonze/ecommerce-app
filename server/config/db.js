8dcd9587eb6e7e5044df3073d41a0e2c5dd43177
Columbus Chiedozie Joseph <144845572+Chijonze@users.noreply.github.com>
2025-02-17 20:27:08 +0100

    Update db.js
    undo nonsense

--- server/config/db.js
+++ server/config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb+srv://admin:Ikechineke1@cluster0.dano4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", {
+    await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
